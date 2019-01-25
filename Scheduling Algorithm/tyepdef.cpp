b01513bb411f3ebb23ca0794fea4b4d845f9ca9f
Sarsinet Varneshon <surana129@gmail.com>
2019-01-25 13:51:46 +0600

    Scheduling Algorithm

---
+++ Scheduling Algorithm/tyepdef.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    typedef  int xyz; // rename int as xyz
+    xyz  p; // declare xyz type variable p
+    p= 10;  //assign an integer into p
+    printf("%d", p); //print
+}
